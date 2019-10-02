ad4321c977ef19a3f646105b7a571d826438a9da
Autímio Meira Brito Filho <autimio.dev@gmail.com>
2019-10-01 23:12:44 -0300

    add middleware global

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
 const projects = [];
+var counter = 0;
+
+server.use((req, res, next) => {
+  counter++;
+  console.log(`Total number of requisitions: ${counter}`);
+  console.time("Time spent requesting");
+  next();
+  console.timeEnd("Time spent requesting");
+});
 
 function checkProjectInArray(req, res, next) {
   const position = projects.findIndex(proj => proj.id === req.params.id);
   const project = projects[position];
   if (!project) {
     return res.status(400).json({
-      error: 'Project not exist'
+      error: "Project not exist"
     });
   }
 
@@ -20,7 +29,7 @@ function checkProjectInArray(req, res, next) {
   return next();
 }
 
-server.post('/projects', (req, res) => {
+server.post("/projects", (req, res) => {
   const { id, title } = req.body;
 
   projects.push({ id, title, tasks: [] });
@@ -28,7 +37,7 @@ server.post('/projects', (req, res) => {
   return res.json(projects);
 });
 
-server.post('/projects/:id/tasks', checkProjectInArray, (req, res) => {
+server.post("/projects/:id/tasks", checkProjectInArray, (req, res) => {
   const { title } = req.body;
 
   projects[req.position].tasks.push(title);
@@ -36,11 +45,11 @@ server.post('/projects/:id/tasks', checkProjectInArray, (req, res) => {
   return res.json(projects);
 });
 
-server.get('/projects', (req, res) => {
+server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.put('/projects/:id', checkProjectInArray, (req, res) => {
+server.put("/projects/:id", checkProjectInArray, (req, res) => {
   const { title } = req.body;
 
   projects[req.position].title = title;
@@ -48,7 +57,7 @@ server.put('/projects/:id', checkProjectInArray, (req, res) => {
   return res.json(projects);
 });
 
-server.delete('/projects/:id', checkProjectInArray, (req, res) => {
+server.delete("/projects/:id", checkProjectInArray, (req, res) => {
   projects.splice(req.position, 1);
 
   return res.json(projects);
